bff3d9f2926c1b7fc9f63b341045a0f5de8c07d1
Dan Buch <daniel.buch@gmail.com>
2013-01-20 15:07:54 -0500

    Updating an older example now that the random libs are more filled in

--- algs4-flips/main.go
+++ algs4-flips/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/rand"
 	"os"
 	"strconv"
-	"time"
-)
 
-import (
-	"github.com/meatballhat/goalgs4"
+	. "github.com/meatballhat/goalgs4"
 )
 
 const USAGE string = "Usage: algs4-flips <nflips>"
@@ -21,19 +17,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	rand.Seed(time.Now().UTC().UnixNano())
-
 	nflips, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		fmt.Println("Wat.", err)
 		os.Exit(2)
 	}
 
-	heads := algs4.NewCounter("heads")
-	tails := algs4.NewCounter("tails")
+	heads := NewCounter("heads")
+	tails := NewCounter("tails")
 
 	for t := 0; t < nflips; t++ {
-		if rand.Float64() > 0.5 {
+		if Random() > 0.5 {
 			heads.Increment()
 		} else {
 			tails.Increment()
